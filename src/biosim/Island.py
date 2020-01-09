ca675e9c4f8ae3809c083c4204af951626c6361e
Sigurd Grøtan <sgrotan@nmbu.no>
2020-01-09 14:19:06 +0100

    Added some code for Island class

--- src/biosim/Island.py
+++ src/biosim/Island.py
@@ -1,2 +1,19 @@
 # -*- coding: utf-8 -*-
 
+class Island:
+
+    def __init__(self, geo_string, params):
+        self.island_dict = self._island_dict_maker(geo_string)
+        self.params = params
+
+    @staticmethod
+    def _island_dict_maker(geo_string):
+        geo_list = [list(line) for line in geo_string.splitlines()]
+        island_dict = {}
+
+        for i, line in enumerate(geo_list):
+            for j, landscape_code in enumerate(line):
+                island_dict[(i, j)] = {"Type": landscape_code, "Fodder": 0}
+
+        return island_dict
+
