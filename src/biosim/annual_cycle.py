e8c6985682b665ddbba847de44ddd08bc1b8ffcb
Sigurd Grøtan <sgrotan@nmbu.no>
2020-01-10 12:00:46 +0100

    Added methods in annual cycle class

--- src/biosim/annual_cycle.py
+++ src/biosim/annual_cycle.py
@@ -3,31 +3,52 @@
 __author__ = 'Daniil Efremov'
 __email__ = 'daniil.vitalevich.efremov@nmbu.no'
 
-
+import animals
+import island
 
 class AnnualCycle:
 
-    def __init__(self, herb_list, carn_list, island_dict):
-        pass
+    def __init__(self, herb_pop_list, carn_pop_list, island):
+        self.herb_pop_list = herb_pop_list
+        self.carn_pop_list = carn_pop_list
+        self.island = island
+        self.num_cycles = 0
 
     def fodder_growth(self):
-        pass
+        self.island.fodder_annual_refill()
 
     def herb_feeding(self):
-        pass
+        for herb in self.herb_pop_list:
+            herb.feed()
 
     def carn_feeding(self):
         pass
 
     def procreation(self):
-        pass
+        for herb in self.herb_pop_list:
+            herb.birth(self.herb_pop_list)
 
     def aging(self):
-        pass
+        for herb in self.herb_pop_list:
+            herb.age()
 
     def weight_loss(self):
-        pass
+        for herb in self.herb_pop_list:
+            herb.annual_weight_loss()
 
     def animal_death(self):
-        pass
+        for herb in self.herb_pop_list:
+            if herb.death()
+                del herb
+
+    def cycle(self, num_years):
+        for _ in range(num_years):
+            self.fodder_growth()
+            self.herb_feeding()
+            self.carn_feeding()
+            self.procreation()
+            self.aging()
+            self.weight_loss()
+            self.animal_death()
+            self.num_cycles += 1
 
