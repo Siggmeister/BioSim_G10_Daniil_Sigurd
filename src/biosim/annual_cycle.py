4ef97baa857fe462926848f000a63dea3e2e3ce6
Daniil Efremov <daniil.efremov98@gmail.com>
2020-01-10 11:31:20 +0100

    made minor changes to annual cycle

--- src/biosim/annual_cycle.py
+++ src/biosim/annual_cycle.py
@@ -10,38 +10,8 @@ class AnnualCycle:
     def __init__(self, herb_list, carn_list, island_dict):
         pass
 
-    #    def fodder_growth(self, island_dict):
-    #
-    #        max_jungle_fodder = 800
-    #        max_savannah_fodder = 300
-    #        alpha = 0.3
-    #
-    #        for key in island_dict:
-    #            if island_dict[key]["Type"] == "J":
-    #                island_dict[key]["Fodder"] = max_jungle_fodder
-    #
-    #            if island_dict[key]["Type"] == "S":
-    #                island_dict[key]["Fodder"] += (alpha *
-    #                (max_savannah_fodder-island_dict[key]["Fodder"]))
-
-    #    def fodder_growth_jungle(self, parameters = None, island_dict):
-    #        if parameters is None:
-    #            parameters = AnnualCycle.param_dict_jungle
-    #
-    #
-    #        for key in island_dict:
-    #            island_dict[key]["Fodder"] = max_jungle_fodder
-    #
-    #    def fodder_growth_savannah(self, parameters = None, island_dict):
-    #        if parameters is None:
-    #            parameters = AnnualCycle.param_dict_savannah
-    #        self.parameters = parameters
-    #
-    #        for key in island_dict:
-    #            island_dict[key]["Fodder"] += (alpha *
-    #                (self.parameters[]-island_dict[key]["Fodder"])) #Endre
-    #
-    #
+    def fodder_growth(self):
+        pass
 
     def herb_feeding(self):
         pass
