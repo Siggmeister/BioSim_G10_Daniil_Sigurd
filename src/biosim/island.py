82ec4637cf0ab6c21ea6227fee942711995c792a
Sigurd Grøtan <sgrotan@nmbu.no>
2020-01-10 11:32:15 +0100

    Added fodder refill functions to Island class

--- src/biosim/island.py
+++ src/biosim/island.py
@@ -5,6 +5,35 @@ class Island:
     def __init__(self, geo_string, params):
         self.island_dict = self._island_dict_maker(geo_string)
         self.params = params
+        self.initial_fodder()
+
+    def initial_fodder(self):
+        f_max_jungle = self.params["J"]["f_max"]
+        f_max_savannah = self.params["S"]["f_max"]
+
+        for loc in self.island_dict:
+            geo_type = self.island_dict[loc]["Type"]
+            if geo_type == "J":
+                self.island_dict[loc]["Fodder"] = f_max_jungle
+            elif geo_type == "S":
+                self.island_dict[loc]["Fodder"] = f_max_savannah
+
+    def fodder_annual_refill(self):
+        f_max_jungle = self.params["J"]["f_max"]
+        f_max_savannah = self.params["S"]["f_max"]
+        alpha = self.params["S"]["alpha"]
+
+        for loc in self.island_dict:
+            current_fodder = self.island_dict[loc]["Fodder"]
+            geo_type = self.island_dict[loc]["Type"]
+            if geo_type == "J":
+                current_fodder = f_max_jungle
+            elif geo_type == "S":
+                current_fodder += (alpha * (f_max_savannah-current_fodder))
+                if current_fodder > f_max_savannah:
+                    current_fodder = f_max_savannah
+            self.island_dict[loc]["Fodder"] = current_fodder
+
 
     @staticmethod
     def _island_dict_maker(geo_string):
@@ -16,4 +45,5 @@ class Island:
                 island_dict[(i, j)] = {"Type": landscape_code, "Fodder": 0}
 
         return island_dict
+
 
