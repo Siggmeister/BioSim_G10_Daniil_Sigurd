e4d10215eea47e53355aa1c052f530ac70a993f8
Sigurd Grøtan <sgrotan@nmbu.no>
2020-01-13 11:16:23 +0100

    Removed parameters from BioSim class

--- src/biosim/simulation.py
+++ src/biosim/simulation.py
@@ -45,26 +45,7 @@ class BioSim:
         img_base should contain a path and beginning of a file name.
         """
 
-        self.landscape_parameters = {"J": {"f_max": 800.0},
-                                     "S": {"f_max": 300.0,
-                                           "alpha": 0.3}}
-
-        self.animal_parameters = {"Herbivore":  {"w_birth": 8.0,
-                                                 "sigma_birth": 1.5,
-                                                 "beta": 0.9,
-                                                 "eta": 0.05,
-                                                 "a_half": 40.0,
-                                                 "phi_age": 0.2,
-                                                 "w_half": 10.0,
-                                                 "phi_weight": 0.1,
-                                                 "mu": 0.25,
-                                                 "lambda": 1.0,
-                                                 "gamma": 0.2,
-                                                 "zeta": 3.5,
-                                                 "xi": 1.2,
-                                                 "omega": 0.4,
-                                                 "F": 10.0}}
-        self.island = island.Island(island_map, self.landscape_parameters)
+        self.island = island.Island(island_map)
         self.herb_list = self.ini_pop_maker(ini_pop, self.island)
         self.carn_list = []
 
