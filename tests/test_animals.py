e31ef32a5f37e0ba8123d7570914b064e330e815
Daniil Efremov <daniil.efremov98@gmail.com>
2020-01-15 11:40:47 +0100

    Added remaining tests for Herbivore and started adding tests for Carnivore

--- tests/test_animals.py
+++ tests/test_animals.py
@@ -5,10 +5,7 @@ __email__ = 'daniil.vitalevich.efremov@nmbu.no'
 
 from src.biosim.animals import *
 from src.biosim.island import *
-from src.biosim.landscape import *
-from src.biosim.annual_cycle import *
 import pytest
-
 
 
 class TestAnimals:
@@ -160,11 +157,9 @@ class TestHerbivore:
         Island._param_changer("J", {"f_max" : -100})
         i_sim = Island(self.geogr)
         s_1 = Herbivore(i_sim, jungle_loc)
-        #s_1.fodder_eaten() #SJEKK UT
 
         with pytest.raises(ValueError):
             s_1.fodder_eaten()
-        #Island.param_changer("J", {"f_max": 800})
 
     def test_weight_gain_properly(self):
         jungle_loc = (2,7)
@@ -193,22 +188,22 @@ class TestHerbivore:
 
         assert not self.stnd_herb.can_birth_occur()
 
-    def test_can_birth_occur_with_5_herbivores_with_1_prob(self, mocker):
+    def test_can_birth_occur_with_2_herbivores_with_1_prob(self, mocker):
         mocker.patch('random.random', return_value=0)
         loc = (2, 7)
         i_sim = Island()
         a_sim = Herbivore(i_sim, loc, weight = 100)
-        for _ in range(5):
+        for _ in range(2):
             i_sim.add_pop_on_loc(loc, a_sim)
 
         assert a_sim.can_birth_occur()
 
-    def test_can_birth_occur_with_5_herbivores_with_0_prob(self, mocker):
+    def test_can_birth_occur_with_2_herbivores_with_0_prob(self, mocker):
         mocker.patch('random.random', return_value=1)
         loc = (2, 7)
         i_sim = Island()
         a_sim = Herbivore(i_sim, loc, weight=100)
-        for _ in range(5):
+        for _ in range(2):
             i_sim.add_pop_on_loc(loc, a_sim)
 
         assert not a_sim.can_birth_occur()
@@ -218,12 +213,125 @@ class TestHerbivore:
         loc = (2, 7)
         i_sim = Island()
         a_sim = Herbivore(i_sim, loc, weight=100)
-        herb_pop_list = [Herbivore(i_sim, loc, weight=100), Herbivore(i_sim, loc, weight=100)]
-        #for _ in range(3):
+        herb_pop_list = [Herbivore(i_sim, loc, weight=100)]
         a_sim.birth(herb_pop_list)
-        #a = AnnualCycle(herb_pop_list, [], i_sim)
+
+        assert len(i_sim.get_herb_list_on_loc(loc)) == 3
+
+    def test_birth_does_not_happen_if_prob_0(self, mocker):
+        mocker.patch('random.random', return_value=1)
+        loc = (2, 7)
+        i_sim = Island()
+        a_sim = Herbivore(i_sim, loc, weight=100)
+        herb_pop_list = [Herbivore(i_sim, loc, weight=100)]
+        a_sim.birth(herb_pop_list)
+
+        assert len(i_sim.get_herb_list_on_loc(loc)) == 2
+
+    def test_annual_weight_loss_decreases_properly(self):
+        loc = (2,7)
+        i = Island()
+        a_sim = Herbivore(i, loc)
+        initial_weight = a_sim.weight
+        a_sim.annual_weight_loss()
+        new_weight = a_sim.weight
+
+        assert initial_weight > new_weight
+
+    def test_death_occurs_if_fitness_is_0(self):
+        loc = (2, 7)
+        i = Island()
+        a_sim = Herbivore(i, loc, weight=0)
+
+        assert a_sim.death()
+
+    def test_death_occurs_if_prob_is_1(self, mocker):
+        mocker.patch('random.random', return_value=0)
+        loc = (2, 7)
+        i_sim = Island()
+        a_sim = Herbivore(i_sim, loc)
+
+        assert a_sim.death()
+
+    def test_death_does_not_occur_if_prob_is_(self, mocker):
+        mocker.patch('random.random', return_value=1)
+        loc = (2, 7)
+        i_sim = Island()
+        a_sim = Herbivore(i_sim, loc)
+
+        assert not a_sim.death()
 
 
-        #assert i_sim.get_herb_pop_on_loc(loc) == 5
-        assert i_sim.get_num_herb_on_loc(loc) == 3
-        #assert len(a.herb_pop_list) == 3
+class TestCarnivore:
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        self.landscape_parameters = {"J": {"f_max": 800.0},
+                                     "S": {"f_max": 300.0,
+                                           "alpha": 0.3}}
+
+        self.animal_parameters = {"Herbivore": {"w_birth": 8.0,
+                                                "sigma_birth": 1.5,
+                                                "beta": 0.9,
+                                                "eta": 0.05,
+                                                "a_half": 40.0,
+                                                "phi_age": 0.2,
+                                                "w_half": 10.0,
+                                                "phi_weight": 0.1,
+                                                "mu": 0.25,
+                                                "lambda": 1.0,
+                                                "gamma": 0.2,
+                                                "zeta": 3.5,
+                                                "xi": 1.2,
+                                                "omega": 0.4,
+                                                "F": 10.0},
+
+                                  "Carnivore": {"w_birth": 6.0,
+                                                "sigma_birth": 1.0,
+                                                "beta": 0.75,
+                                                "eta": 0.0125,
+                                                "a_half": 60.0,
+                                                "phi_age": 0.4,
+                                                "w_half": 4.0,
+                                                "phi_weight": 0.4,
+                                                "mu": 0.4,
+                                                "lambda": 1.0,
+                                                "gamma": 0.8,
+                                                "zeta": 3.5,
+                                                "xi": 1.1,
+                                                "omega": 0.9,
+                                                "F": 50.0,
+                                                "DeltaPhiMax": 10.0}}
+
+        self.geogr = """\
+                              OOOOOOOOOOOOOOOOOOOOO
+                              OOOOOOOOSMMMMJJJJJJJO
+                              OSSSSSJJJJMMJJJJJJJOO
+                              OSSSSSSSSSMMJJJJJJOOO
+                              OSSSSSJJJJJJJJJJJJOOO
+                              OSSSSSJJJDDJJJSJJJOOO
+                              OSSJJJJJDDDJJJSSSSOOO
+                              OOSSSSJJJDDJJJSOOOOOO
+                              OSSSJJJJJDDJJJJJJJOOO
+                              OSSSSJJJJDDJJJJOOOOOO
+                              OOSSSSJJJJJJJJOOOOOOO
+                              OOOSSSSJJJJJJJOOOOOOO
+                              OOOOOOOOOOOOOOOOOOOOO"""
+
+        self.carn_params = self.animal_parameters["Carnivore"]
+        self.island = Island(self.geogr)
+        self.loc = (2, 7)
+
+
+    def test_fitness_change_for_set_weight(self):
+
+        s_1 = Carnivore(self.island, self.loc, weight=3)
+        s_2 = Carnivore(self.island, self.loc, weight=5)
+        s_3 = Carnivore(self.island, self.loc, weight=7)
+        s_1.fitness_change()
+        s_2.fitness_change()
+        s_3.fitness_change()
+
+        assert s_1.fitness == pytest.approx(0.401, 0.01)
+        assert s_2.fitness == pytest.approx(0.598, 0.01)
+        assert s_3.fitness == pytest.approx(0.768, 0.01)
+        # Sjekk med sannsynlighet
